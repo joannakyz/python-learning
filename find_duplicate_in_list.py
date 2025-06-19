cd612889bb1227d22951f646f44b59d65aed2437
Ioanna Kyzeridou <joannakyz45@gmail.com>
2025-06-19 11:03:41 +0300

    searches for duplicates in a list and prints

---
+++ find_duplicate_in_list.py
@@ -0,0 +1,9 @@
+def find_duplicates(lst):
+    seen = set()
+    duplicates = set()
+    for item in lst:
+        if item in seen:
+            duplicates.add(item)
+        else:
+            seen.add(item)
+    return list(duplicates)
