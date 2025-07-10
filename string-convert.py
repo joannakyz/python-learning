5d15bd8f45b4d7f154169f1c8d1bb034fe0df109
Ioanna Kyzeridou <joannakyz45@gmail.com>
2025-07-10 15:14:07 +0300

    string-convert.py
    practice some string conversions and joins

---
+++ string-convert.py
@@ -0,0 +1,14 @@
+print("int: " + str(5))
+print("float: " + str(5.1))
+print("boolean: " + str(True))
+print("list: " + str([1,2]))
+print("tuple: " + str((1,2)))
+print("set: " + str({1,2}))
+print("dictionary: " + str({1:2, 5:6}))
+print("dictionary: " + str({"name": "Jo", "surname":"Kyz"}))
+print("list: " + str(list("The cat jumped over the lazy robot.")))
+
+
+print(" ".join(["a", "b", "c"]))
+print(",".join({'a', 'b', 'c'}))
+print("#".join({"a":1, "b":10}))
