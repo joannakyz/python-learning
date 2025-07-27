e794cbd1683ce41602b0aa26e0e444c41cab344c
Ioanna Kyzeridou <joannakyz45@gmail.com>
2025-07-27 16:09:18 +0300

    Create complex-num.py

---
+++ complex-num.py
@@ -0,0 +1,11 @@
+#complex numebers
+
+z1 = 1 + 2j
+z2 = 5 - 8j
+
+addition = z1 + z2
+multiplication = z1 * z2
+conj_z1 = z1.conjugate()
+print(f"+: {addition}")
+print(f"x: {multiplication}")
+print(f"conjugate: {conj_z1}") #συζηγής
