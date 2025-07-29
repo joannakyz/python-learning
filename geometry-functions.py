5631db9918aa0c0b52005c42ec32756675f789ee
Ioanna Kyzeridou <joannakyz45@gmail.com>
2025-07-29 18:52:55 +0300

    geometry-functions.py

---
+++ geometry-functions.py
@@ -0,0 +1,17 @@
+def triange_surface(base, height):
+    return base * height / 2
+
+def square_perimetre(side):
+    return 4 * side
+
+def square_surface(side):
+    return side ** 2
+
+def circle_perimetre(radious):
+    pi = 3.14159
+    return 2 * pi * radious
+
+def circle_surface(radious):
+    pi = 3.14159
+    return pi * (radious ** 2)
+
