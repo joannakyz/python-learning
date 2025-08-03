21eb67a650ab7044d061993022a57ffef4557330
Ioanna Kyzeridou <joannakyz45@gmail.com>
2025-08-03 19:48:20 +0300

    binary_search_list.py

--- binary_search_list.py
+++ binary_search_list.py
@@ -14,3 +14,37 @@ def binary_search(arr, num):
             start = position + 1
 
     print("Not found.")
+
+
+def binary_search(a_list, element, start, end):
+    a_list = sorted(a_list)  
+
+    if start > end:
+        return -1  
+
+    pos = (start + end) // 2
+
+    if element == a_list[pos]:
+        return pos
+    elif element < a_list[pos]:
+        return binary_search(a_list, element, start, pos - 1)
+    else:
+        return binary_search(a_list, element, pos + 1, end)
+
+
+def binary_search(arr, ele):
+    arr_sort = sorted(arr)
+    start = 0
+    end = len(arr_sort) - 1
+
+    while start <= end:
+        pos = (start + end) // 2
+
+        if arr_sort[pos] == ele:
+            return pos + 1  
+        elif arr_sort[pos] > ele:
+            end = pos - 1
+        else:
+            start = pos + 1
+
+    return -1  
